dae041a3a7921295720a4ac9e073cde8e32bf71b
Vlad412 <vlad_pro@live.fr>
2020-02-04 17:35:06 +0100

    unstaged idea and create migrations

---
+++ database/migrations/2020_02_04_150423_transactions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Transactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->string('crypto');
+            $table->integer('purchase_value');
+            $table->integer('purchase_quantity');
+            $table->boolean('sold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
