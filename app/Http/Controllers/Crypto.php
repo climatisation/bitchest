70d823a75fac2851c302c448d6988d8952ac5c29
Vlad412 <vlad_pro@live.fr>
2020-02-06 11:44:38 +0100

    first crypto api test

--- app/Http/Controllers/Crypto.php
+++ app/Http/Controllers/Crypto.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 
 class Crypto extends Controller
 {
+
+    public $client;
+    public $response;
+
     public function __construct()
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
-        $this->middleware('auth');
+        $this->client = new \GuzzleHttp\Client();
+        $this->response = $this->client->request('GET', 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,BCH,ADA,LTC,XEM,XLM,MIOTA,DASH&tsyms=EUR', ['authorization' => '73741100522c6cd83adc3958daeab029b3c73e924eb4ab91aa3a34e0773c24f5']);
+        // $this->middleware('auth');
+    }
+
+    public function index() {
+        echo $this->response->getBody();
     }
 }
